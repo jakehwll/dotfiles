10672bbce6fb009b389c648fb142406147097059
Jake <jake@hwll.me>
2023-07-12 15:23:17 +1000

    fix: resolve install order of `oh-my-zsh`

--- install.sh
+++ install.sh
@@ -3,15 +3,6 @@
 CURRENT_DIR=`pwd`
 ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 
-mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions
-fi
-if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting
-fi
-cd "$CURRENT_DIR"
-
 # Backup our zsh config
 echo "👀 Backing up zsh config"
 mv ~/.zshrc ~/.zshrc.old
@@ -29,6 +20,15 @@ if [ ! -d "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
   echo "👀 Installing oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+
+mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions
+fi
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
+fi
+cd "$CURRENT_DIR"
 
 # Get the shell and change it to zsh if it isn't already
 if [ "$SHELL" != "$(which zsh)" ]; then
