3c732a03fc3e54fddb7454212850f77ec2b6f96f
Jake Howell <jake@hwll.me>
2025-10-25 14:11:23 +1100

    chore: redact old commands

--- .zshrc
+++ .zshrc
@@ -1,7 +1,3 @@
-# ===================================== #
-# == Jake Howell's ZSH configuration == #
-# ===================================== #
-
 # Path to oh-my-zsh configuration.
 export ZSH=~/.oh-my-zsh
 
@@ -24,24 +20,9 @@ source ${ZSH_PLUGINS_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # Hook our ZSH install.
 source $ZSH/oh-my-zsh.sh
 
-# If vscode is installed, set it to the default editor.
-if command -v code &> /dev/null; then
-  export EDITOR='code -w'
-fi
-
 # If `direnv` is installed, add it's hook.
 if command -v direnv &> /dev/null; then
   eval "$(direnv hook zsh)"
 fi
 
-# ===================================== #
-# Jake Howell's Magic
-# ===================================== #
-
-export EDITOR='code --wait'
-
-alias g="git"
-alias b="bun"
-alias y="yarn"
-alias p="pnpm"
-alias py="python3"
+export EDITOR='cursor --wait'
--- install.sh
+++ install.sh
@@ -1,29 +1,20 @@
 #!/bin/zsh
 
-CURRENT_DIR=`pwd`
-ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-
-# Check if zsh is installed and change it to zsh if its not already
-if [ ! -x "$(command -v zsh)" ]; then
-  echo "😠 `zsh` is not installed"
-  exit 1
-fi
-
 # Check if ohmyzsh is installed and install it if its not
 if [ ! -d "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
-  echo "😠 oh-my-zsh is not installed"
   echo "👀 Installing oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions
 fi
+
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting
 fi
-cd "$CURRENT_DIR"
 
 mv $HOME/.zshrc $HOME/.zshrc.pre-dotfiles
 cp ./.zshrc $HOME/.zshrc
